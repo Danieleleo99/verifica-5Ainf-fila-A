47b8d7c4e98e3e7907536b78dacfe10d7480db75
daniele leo <danieleleo16@gmail.com>
2018-11-17 10:14:29 +0100

    verifica daniele leo completata

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -12,16 +12,51 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(500,500);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb = new StringBuilder ("<html>");
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<h1><b>SSH first aid</b></h1>");
+		sb.append("<div style='display:table'>");
+		//this is the first row is about the table title
+		sb.append("<div style='display:table-row'>");
+		sb.append("<div style='display:table cell'>");
+		sb.append("alias");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table cell'>");
+		sb.append("hostname");
+		sb.append("</div>");
+		//this is the row n.2
+		sb.append("<div style='display:table-row'>");
+		sb.append("<div style='display:table cell'>");
+		sb.append("raspberry");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table cell'>");
+		sb.append("server.marconi.it");
+		sb.append("</div>");
+		//this is the row n.3
+		sb.append("<div style='display:table-row'>");
+		sb.append("<div style='display:table cell'>");
+		sb.append("coreswitch");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table cell'>");
+		sb.append("10.0.100.1");
+		sb.append("</div>");
+		
+		
+		
+		
 		
 		// TODO Convertire lo StringBuilder in String
 		
+		String HtmlText = sb.toString();
+		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(HtmlText));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
